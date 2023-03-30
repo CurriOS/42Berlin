5ebddc2dba7bc04cbb4cc757934e2a5bcc162ae0
CurriOS <kkononov.denis@gmail.com>
2023-03-30 13:02:06 +0200

    1

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,23 @@
+int	ft_recursive_factorial(int	nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb > 0)
+	{
+		result *= nb * ft_recursive_factorial(nb - 1);
+		nb--;
+	}
+	if (nb < 0)
+		return (0);
+	return (result);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d", ft_recursive_factorial(9));
+}
+*/
