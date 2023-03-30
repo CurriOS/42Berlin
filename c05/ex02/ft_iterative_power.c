272690ff25d0828f1c2d54277ec7c5f52abc4135
CurriOS <kkononov.denis@gmail.com>
2023-03-30 13:07:54 +0200

    2

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,25 @@
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	while (power > 1)
+	{
+		result *= nb;
+		power--;
+	}
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (result);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d", ft_iterative_power(5, 3));
+}
+*/
